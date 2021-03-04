d266c21596c6937f9228f40a597495ffd1ae1946
koutika ange marie <koutikaaem@gmail.com>
2021-03-04 19:21:33 +0100

    Set tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/* postcss.config.js */
+module.exports = {
+  plugins: [
+    require("tailwindcss")("./app/javascript/stylesheets/tailwind.config.js"),
+    require("postcss-import"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
+    }),
+  ],
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.vue",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
